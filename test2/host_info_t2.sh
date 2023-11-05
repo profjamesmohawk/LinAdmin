a7f0277e7fc070519f941d30c17632b2da312bbd
james <james.long5@mohawkcollege.ca>
2023-11-05 16:21:17 -0500

    updated test 2

--- test2/host_info_t2.sh
+++ test2/host_info_t2.sh
@@ -1,57 +1,57 @@
 #!/bin/bash
 
 OUT_FILE=$(hostname)_report_t2.html
+rm $OUT_FILE 2>/dev/null
 
 # map stdout to OUT_FILE
 #
-rm $OUT_FILE 2>/dev/null
-exec 1<> $OUT_FILE
+#exec 1<> $OUT_FILE
 
 ################ functions to spit html ################################
 #
-function spit_start {
-	echo "<tr>"
-	echo "<td valign=\"top\"> $1 </td>"
-	echo "<td><pre>"
-	echo " "
-
-	# tell the user what we are up to
-	echo "Collecting Info for $1" | sed -e 's/<.*>//g' >&2
-}
-
-function spit_end {
-	echo " "
-	echo "</pre>"
-	echo "</td>"
-	echo "</tr>"
-}
 
 function spit_pre {
 	echo "<html>"
 	echo "<head>"
 	echo "<style type=\"text/css\">"
 	echo "table { margin: 1em; border-collapse: collapse; }"
+	echo "pre { overflow: auto; display: block; width: 90%; border: 1px solid #ccc; padding: 3px; background: #ece9d8; margin-top: 0px; margin-left: 5px; border-radius: 5px; }"
 	echo "td, th { padding: .3em; border: 2px #ccc solid; }"
 	echo "</style>"
 	echo "</head>"
 
-	echo "<body><table>"
+	echo "<body>"
 }
 
 function spit_post {
-	echo "</table></body></html>"
+	echo "</body></html>"
 }
 
 function spit_title {
 	echo "<h2> $1 </h2>"
 }
 
-function spit_section_header {
-	echo "<tr><td colspan=\"2\"> $1 </td></tr>"
+
+function start {
+	echo ""
+	#echo "<strong>${1}</strong>"
+	echo "${1}"
+	echo -n "<pre>" 
+}
+function end {
+	echo "</pre>"
+}
+
+function e3 {
+	echo "<hr>"
+	#echo "<h3>${1}</h3>"
+	#echo "<strong>${1}</strong><br>"
+	#echo "<br>"
 }
 #
 ################# end html spitters ######################################
 
+{
 #
 # are we root?
 #
@@ -78,70 +78,74 @@ spit_pre
 
 spit_title "COMP-10018 Host Info Report - Test 2"
 
-spit_start Hostname
+start "Host Info:"
 hostname
 date
-spit_end
+end
 
-spit_start "Is this a fresh VM?<br><em>boot history</em>"
+start "Is this a fresh VM? (boot history)"
 grep -h 'Command line: BOOT' /var/log/messages* | cut -c 1-12 | grep -v Jun| sort -M
-spit_end
+end
 
 
-spit_section_header "Part B: Backup (2 points)"
+e3 "Backup (5 points)"
 
-spit_start "tar ball exists (1 point)<br>should have around 700 files"
+start  "/tmp/etc.tar should have around 700 files (1 point)"
 tar tf /tmp/etc.tar | wc -l
-spit_end
+end
 
-spit_start "tar ball containts /etc (1 point)<br>look at first 4 files"
-tar tf /tmp/etc.tar | head -4
-spit_end
+start  "books.cpio: correct files, dates (2 points)"
+cpio -tv < /tmp/books.cpio | head -4
+end
 
-spit_section_header "Part C: NFS (5 points)"
+start "foo.tar correct files, dates (2 points)"
+tar vtf /tmp/foo.tar | head -5
+end
 
-spit_start "NFS Startup (1 point)"
-systemctl is-enabled nfs-server 
-spit_end
 
-spit_start "NFS Connecton from w01 (1 point)"
+e3 "NFS (5 points)"
+
+
+start "NFS Connecton from w01 (1 point)"
 netstat -a | grep 'nfs.*w01'
-spit_end
+end
 
-spit_start "/nfs/w01 in exports (2 points)"
+start "/nfs/w01 in exports (2 points)"
 cat /etc/exports
-spit_end
+end
 
-spit_start "Test files in /nfs/w01 (1 point)"
+start "Test files in /nfs/w01 (2 points)"
 ls -l /nfs/w01
-spit_end
+end
 
-spit_section_header "Part D: Logging (5 points)"
+e3 "Logging (5 points)"
 
-spit_start "remote logging enabled (2 points)"
+start "remote logging enabled (1 point)"
 grep -e imudp -e imtcp /etc/rsyslog.conf | grep -v '^#'
-spit_end
+end
 
-spit_start "anything in /var/log/httpd.err (1 point)"
-wc -l /var/log/httpd.err 
-spit_end
+start "contents of /var/log/web.log (2 points)"
+tail -5 /var/log/web.log
+end
 
-spit_start "http errors from w01 in <br>/var/log/httpd.err (2 points)"
-grep w01 /var/log/httpd.err | tail -2
-spit_end
-
-spit_section_header "Part E: Incremental Backup (2 points)"
-
-spit_start "Incremental tar ball exists (1 point)"
-tar tf /tmp/changes.tar | wc -l
-spit_end
-
-spit_start "Incremental tar ball contents (1 point)"
-tar tf /tmp/changes.tar | tail -20
-spit_end
+start "authpriv from w01 in /var/log/secure (2 points)"
+grep w01 /var/log/secure | tail -2
+end
 
 echo "<!-- report_id=$(uuidgen) -->"
-$date
+
+echo "<!-- altenc"
+} > $OUT_FILE
+
+# squirrel away a copy in a comment
+#
+TF=$(mktemp)
+cat $OUT_FILE | gzip | base64 > $TF
+cat $TF >> $OUT_FILE
+
+{
+echo "altenc -->"
 
 spit_post
 
+} >> $OUT_FILE
